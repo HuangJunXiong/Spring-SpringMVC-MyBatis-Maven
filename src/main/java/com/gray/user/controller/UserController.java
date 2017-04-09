c7dd4a71d83a9f4f85016adb3082f30a9fed810e
HuangJunXiong <923383921@qq.com>
2017-04-09 23:59:20 +0800

    添加事务测试代码

--- src/main/java/com/gray/user/controller/UserController.java
+++ src/main/java/com/gray/user/controller/UserController.java
@@ -34,33 +34,33 @@ public class UserController {
 	@Qualifier("remindServiceImpl")
 	private RemindService remindService;
 
-@RequestMapping("/dologin.do") //url
-@Log(oper="user login")
-public String dologin(User user, Model model){
-	logger.info("login ....");
-	String info = loginUser(user);
-	if (!"SUCC".equals(info)) {
-		model.addAttribute("failMsg", "User does not exist or password error!");
-		return "/jsp/fail";
-	}else{
-		model.addAttribute("successMsg", "login Succ!");//返回到页面说夹带的参数
-		model.addAttribute("name", user.getUsername());
-		return "/jsp/success";//返回的页面
+	@RequestMapping("/dologin.do") //url
+	@Log(oper="user login")
+	public String dologin(User user, Model model){
+		logger.info("login ....");
+		String info = loginUser(user);
+		if (!"SUCC".equals(info)) {
+			model.addAttribute("failMsg", "User does not exist or password error!");
+			return "/jsp/fail";
+		}else{
+			model.addAttribute("successMsg", "login Succ!");//???????????
+			model.addAttribute("name", user.getUsername());
+			return "/jsp/success";//?????
+		}
 	}
-  }
 
-@RequestMapping("/logout.do")
-@Log(oper="user logout")
-public void logout(HttpServletRequest request,HttpServletResponse response) throws IOException{
-    Subject subject = SecurityUtils.getSubject();
-    if (subject != null) {
-    	try{
-            subject.logout();
-    	}catch(Exception ex){
-    	}
-    }
-    response.sendRedirect("/index.jsp");
-}
+	@RequestMapping("/logout.do")
+	@Log(oper="user logout")
+	public void logout(HttpServletRequest request,HttpServletResponse response) throws IOException{
+		Subject subject = SecurityUtils.getSubject();
+		if (subject != null) {
+			try{
+				subject.logout();
+			}catch(Exception ex){
+			}
+		}
+		response.sendRedirect("/index.jsp");
+	}
 
 //@RequestMapping(value="/list", method=RequestMethod.GET)
 //@ResponseBody
@@ -73,56 +73,55 @@ public void logout(HttpServletRequest request,HttpServletResponse response) thro
 //	return userService.findByUserName(username);
 //}
 
-private String loginUser(User user) {
- 		if (isRelogin(user)) return "SUCC"; // 如果已经登陆，无需重新登录
- 		
- 		return shiroLogin(user); // 调用shiro的登陆验证
-}
-private String shiroLogin(User user) {
-		// 组装token，包括客户公司名称、简称、客户编号、用户名称；密码
-	UsernamePasswordToken token = new UsernamePasswordToken(user.getUsername(), user.getPassword().toCharArray(), null); 
-	token.setRememberMe(true);
-	
-	// shiro登陆验证
-	try {
-		SecurityUtils.getSubject().login(token);
-	} catch (UnknownAccountException ex) {
-		return "User does not exist or password error!";
-	} catch (IncorrectCredentialsException ex) {
-		return "User does not exist or password error!";
-	} catch (AuthenticationException ex) {
-		ex.printStackTrace();
-		return ex.getMessage(); // 自定义报错信息
-	} catch (Exception ex) {
-		ex.printStackTrace();
-		return "Internal error, please try again!";
-	}
-	return "SUCC";
-}
+	private String loginUser(User user) {
+		if (isRelogin(user)) return "SUCC"; // ?????????????
 
-private boolean isRelogin(User user) {
-	Subject us = SecurityUtils.getSubject();
-		// 验证通过的时候，需要再次核对参数，如果所有参数均未改变，则无需登录（可以保持住session，避免潜在错误！！- Steven 2014-12-13）
-		if (us.isAuthenticated()) {
-	       return true; // 参数未改变，无需重新登录，默认为已经登录成功
+		return shiroLogin(user); // ??shiro?????
+	}
+	private String shiroLogin(User user) {
+		// ??token?????????????????????????
+		UsernamePasswordToken token = new UsernamePasswordToken(user.getUsername(), user.getPassword().toCharArray(), null);
+		token.setRememberMe(true);
+
+		// shiro????
+		try {
+			SecurityUtils.getSubject().login(token);
+		} catch (UnknownAccountException ex) {
+			return "User does not exist or password error!";
+		} catch (IncorrectCredentialsException ex) {
+			return "User does not exist or password error!";
+		} catch (AuthenticationException ex) {
+			ex.printStackTrace();
+			return ex.getMessage(); // ???????
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return "Internal error, please try again!";
 		}
-		return false; // 需要重新登陆
-}
+		return "SUCC";
+	}
+
+	private boolean isRelogin(User user) {
+		Subject us = SecurityUtils.getSubject();
+		if (us.isAuthenticated()) {
+			return true; // ??????????????????????
+		}
+		return false; // ??????
+	}
 	@RequestMapping("/register.do")
 	@Log(oper="user register")
-    @ResponseBody
+	@ResponseBody
 	public ServiceParam register(User user, Model model){
 		if(!StringUtils.isEmpty(user.getUsername())){
-		    user.setRole("user");
-            User userDb = userService.findByUserName(user.getUsername());
-            User userDB = userService.findByEmail(user.getEmail());
-            if (userDb != null)return new ServiceParam("user exist!");
-            if(userDB != null) return new ServiceParam("email exist!");
-            userService.insertUser(user);
-            // 使用消息队列通知发邮件
+			user.setRole("user");
+			User userDb = userService.findByUserName(user.getUsername());
+			User userDB = userService.findByEmail(user.getEmail());
+			if (userDb != null)return new ServiceParam("user exist!");
+			if(userDB != null) return new ServiceParam("email exist!");
+			userService.insertUser(user);
+			// ???????????
 			remindService.sendRegisterRemind(user);
-            return new ServiceParam(null,"register Succ!",true);
-        }
-        return new ServiceParam("paramErr!");
+			return new ServiceParam(null,"register Succ!",true);
+		}
+		return new ServiceParam("paramErr!");
 	}
 }
